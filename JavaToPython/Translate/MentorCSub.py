47fce1c0bcd78584437ef5e793680b8ac62dfc53
Yanic Mainville <ymain031@uOttawa.ca>
2022-10-21 09:57:51 -0400

    Revert "Pushed bash script to convert a bunch of files at once"
    This reverts commit 09bdb968adb7e633aeb80c06910c42d0c4b2c38e.

--- JavaToPython/Translate/MentorCSub.py
+++
@@ -1,9 +0,0 @@
-from MentorC import MentorC
-
-class MentorCSub(MentorC):
-    def __init__(self, aName):
-        super().__init__(aName)
-
-    def delete(self):
-        super().delete()
-
--- JavaToPython/translateAll.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "" > output.txt
-for filename in Translate/*.java; do
-    [ -e "$filename" ] || continue
-    Output=$(txl $filename umpleJavaToPython.txl > ${filename%%.*}".py" -q 2>> output.txt)
-done
-echo "Finished translating files!"
-read
